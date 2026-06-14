1b8372d429cb7c7df3e4f38a200ba19dc1ba0fe0
Kat Winter <katarawinter@gmail.com>
2026-06-14 12:34:24 +0100

    Added an error page

--- src/main/java/me/winterbelle/prompvault/utils/helpers/configs/SecurityConfig.java
+++ src/main/java/me/winterbelle/prompvault/utils/helpers/configs/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -26,12 +25,16 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) {
         return http
-//                .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(httpForm -> {
                     httpForm.loginPage("/req/login").permitAll();
                     httpForm.loginProcessingUrl("/req/login");
                     httpForm.defaultSuccessUrl("/home", true);
                 })
+//                .exceptionHandling(exception -> {
+//                    exception.accessDeniedHandler((request, response, accessDeniedException) -> {
+//                        response.sendRedirect("/home");
+//                    });
+//                })
                 .authorizeHttpRequests(registry -> {
                     registry.requestMatchers("/req/**", "/css/**", "/js/**").permitAll();
                     registry.requestMatchers("/admin/**").hasRole("ADMIN");
